5c889fb1bae35a9d1a6777e8d95d56a1cbec7f02
Reynanda Putra Pratama <reynandapp1997@gmail.com>
2019-09-14 12:11:55 +0700

    add testing

---
+++ test/index.test.js
@@ -0,0 +1,17 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const server = require('../app');
+
+const expect = chai.expect;
+chai.use(chaiHttp);
+
+describe('User', () => {
+    it('should return 200 status code', done => {
+        chai.request(server)
+            .get('/users')
+            .end((error, result) => {
+                expect(result.status).eql(200);
+                done();
+            });
+    });
+});
